944db232948bb826bd597c151243265238234b8f
Yann Pringault <yann@cinq-s.com>
2016-12-08 10:17:55 +0100

    Homepage text to match the purpose of this site

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to qatataq radio!</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Soon available.
         </p>
       </div>
     );
