759c1154557e49340a045e7a649bd7330d5875c9
Clément Sirieix <clem.sirieix@gmail.com>
2016-12-09 00:22:25 +0100

    init player

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Soundcloud from 'soundcloud';
+import _ from 'lodash';
 
 import logo from './logo.svg';
+import Player from './Player';
 import './App.css';
 import apiConfig from '../apiConfig.json';
 
@@ -10,32 +12,32 @@ class App extends Component {
    * The constructor will init the SoundCloud library and set the initial state of the component
    */
   constructor() {
-      super();
-      Soundcloud.initialize(apiConfig);
-      this.state = {
-          playlist: [],
-          loading: true,
-          error: false
-      }
+    super();
+    Soundcloud.initialize(apiConfig);
+    this.state = {
+      playlist: [],
+      loading: true,
+      error: false
+    }
   }
   /**
    * When the component will mount, for a playlist url we fetch the tracks of this playlist
    */
   componentWillMount() {
-      Soundcloud.resolve('https://soundcloud.com/qatataq/sets/seleqta004-maazel-qatataq-birthday')
-        .then(response => Soundcloud.get(`/playlists/${response.id}`))
-        .then(response => {
-            this.setState({
-                playlist: response.tracks,
-                loading: false
-            });
-        })
-        .catch(error => {
-            this.setState({
-                loading: false,
-                error: true
-            });
+    Soundcloud.resolve('https://soundcloud.com/qatataq/sets/seleqta004-maazel-qatataq-birthday')
+      .then(response => Soundcloud.get(`/playlists/${response.id}`))
+      .then(response => {
+        this.setState({
+          playlist: _.shuffle(response.tracks),
+          loading: false
         });
+      })
+      .catch(error => {
+        this.setState({
+          loading: false,
+          error: true
+        });
+      });
   }
   render() {
     return (
@@ -44,9 +46,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to qatataq radio!</h2>
         </div>
-        <p className="App-intro">
-          Soon available.
-        </p>
+        <Player apiConfig={apiConfig} playlist={this.state.playlist}/>
       </div>
     );
   }
